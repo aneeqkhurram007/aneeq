20ad73e09d67e12158c3ca118a3224d4c1d28daf
Aneeq Khurram <aneeqkhurram007@yahoo.com>
2021-11-23 19:40:51 +0500

    My intro

---
+++ README.md
@@ -0,0 +1,2 @@
+Hi there
+run npx aneeqkhurram007 and connect with me
---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+console.log(`
+
+Aneeq Khurram
+
+Self-learned and creative React developer having experience of more than a year in Node JS, 
+MERN, and NodeCLI automation.
+
+Connect with me
+
+Github: https://github.com/aneeqkhurram007
+Twitter: aneeqkhurram007
+Instagram: aneeqkhurram007
+
+`);
