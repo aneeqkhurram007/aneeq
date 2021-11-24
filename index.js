c4ba3ba89b5ba3a7b5eea71ca0f0b4194412a9df
Aneeq Khurram <aneeqkhurram007@yahoo.com>
2021-11-24 08:16:03 +0500

    Colors added

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 #!/usr/bin/env node
-const welcome = require("cli-welcome");
-const { name, version, description } = require("./package.json");
+const welcome = require(`cli-welcome`);
+const { name, version, description } = require(`./package.json`);
+const chalk = require(`chalk`);
 welcome({
-  title: name,
-  tagLine: "Mr. React",
+  title: `${chalk.hex(`#FADC00`)(` Aneeq Khurram `)}`,
+  tagLine: `Mr. React`,
   description: description,
   version: version,
   clear: true,
-  bgColor: "FADC00",
-  color: "FFF",
+  bgColor: `FADC00`,
+  color: `FFF`,
   bold: true,
 });
-console.log(`
+console.log(`${chalk.green
+  .italic(`Self-learned and creative React developer having experience of more than a year in Node JS, 
+MERN, and NodeCLI automation.`)}
 
-Aneeq Khurram
+${chalk.white.inverse(` Connect with me `)}
 
-Self-learned and creative React developer having experience of more than a year in Node JS, 
-MERN, and NodeCLI automation.
-
-Connect with me
-
-Github: https://github.com/aneeqkhurram007
-Twitter: aneeqkhurram007
-Instagram: aneeqkhurram007
+Github: ${chalk.dim(`https://github.com/aneeqkhurram007`)}
+Twitter: ${chalk.dim(`aneeqkhurram007`)}
+Instagram: ${chalk.dim(`aneeqkhurram007`)}
 
 `);
